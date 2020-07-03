41d7a6945e76417dee5bf5e1c95469364e826110
Fatimaalbahar <albaharfatema18@gmail.com>
2020-07-03 19:58:25 +0300

    homework

---
+++ MarioParty/MarioParty/Models/Player.swift
@@ -0,0 +1,32 @@
+//
+//  Player.swift
+//  MarioParty
+//
+//  Created by Fatima Al Bahar on 7/2/20.
+//  Copyright © 2020 Dalal Alhazeem. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var name: String
+    func bgName() -> String
+    {
+        return name + "BG"
+    }
+    
+    func musicName() -> String {
+        return name + ".WAV"
+    }
+}
+
+let players : [Player] = [
+
+    Player(name: "Mario"),
+    Player(name: "Bowser"),
+    Player(name: "Luigi"),
+    Player(name: "Peach"),
+    Player(name: "Waluigi"),
+    Player(name: "Yoshi"),
+
+]
